745294aef51cad7e2d812e9af59a99d9bb8ce856
Mac Chaffee <machaffe@renci.org>
2022-12-05 17:31:28 -0500

    Add usage example to readme

--- README.md
+++ README.md
@@ -4,6 +4,50 @@ This repo contains a parser for the Stream Processing Offload Protocol (SPOP), g
 
 Official protocol description: https://github.com/haproxy/haproxy/blob/v2.7.0/doc/SPOE.txt
 
+## Usage
+
+Right now, this repo only contains the raw generated parser. In the future, I'll add a nicer wrapper API around the generated parser (work in progress). Until then, you can perform rudimentary parsing with the generated parser directly:
+
+```go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kaitai-io/kaitai_struct_go_runtime/kaitai"
+	"github.com/mac-chaffee/generated-spoe/parser"
+)
+
+func parse(filename string) *parser.Spop {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	stream := kaitai.NewStream(f)
+	spop := parser.NewSpop()
+	if err = spop.Read(stream, nil, spop); err != nil {
+		panic(err)
+	}
+	return spop
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <binary file>")
+	}
+	spop := parse(os.Args[1])
+	fmt.Printf("Frame Length: %d\n", spop.FrameLen)
+}
+```
+
+Example:
+
+```
+$ go run main.go resources/notify_frame.bin
+Frame Length: 883
+```
+
 ## Generation
 
 1. Follow these instructions to install kaitai: https://kaitai.io/#quick-start
--- main.go
+++ main.go
@@ -2,83 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/kaitai-io/kaitai_struct_go_runtime/kaitai"
+	"github.com/mac-chaffee/generated-spoe/parser"
 )
 
-func encodeVarint(b []byte, i int) (int, error) {
-	if len(b) == 0 {
-		return 0, fmt.Errorf("encode varint: insufficient space in buffer")
-	}
-
-	if i < 240 {
-		b[0] = byte(i)
-		return 1, nil
-	}
-
-	n := 0
-
-	b[n] = byte(i) | 240
-	n++
-	i = (i - 240) >> 4
-	for i >= 128 {
-		if n > len(b)-1 {
-			return 0, fmt.Errorf("encode varint: insufficient space in buffer")
-		}
-
-		b[n] = byte(i) | 128
-		n++
-		i = (i - 128) >> 7
-	}
-
-	if n > len(b)-1 {
-		return 0, fmt.Errorf("encode varint: insufficient space in buffer")
-	}
-
-	b[n] = byte(i)
-	n++
-
-	return n, nil
-}
-
-func decodeVarint(b []byte) (int, int, error) {
-	if len(b) == 0 {
-		return 0, 0, fmt.Errorf("decode varint: unterminated sequence")
-	}
-	val := int(b[0])
-	off := 1
-	if val < 240 {
-		return val, 1, nil
-	}
-	r := uint(4)
-	for {
-		if off > len(b)-1 {
-			return 0, 0, fmt.Errorf("decode varint: unterminated sequence")
-		}
-
-		v := int(b[off])
-		val += v << r
-		off++
-		r += 7
-
-		if v < 128 {
-			break
-		}
-	}
-	return val, off, nil
-}
-
-func main() {
-	var intmap map[int][]byte = map[int][]byte{
-		0:   {0x00},
-		1:   {0x01},
-		239: {0xef},
-		// 240: {0xff, 0xf0},
-		662: {0xfd, 0x1a},
-	}
-	fmt.Println(decodeVarint(intmap[662]))
-	buf := make([]byte, 4)
-	_, err := encodeVarint(buf, 300000)
+func parse(filename string) *parser.Spop {
+	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%x", buf)
+	stream := kaitai.NewStream(f)
+	spop := parser.NewSpop()
+	if err = spop.Read(stream, nil, spop); err != nil {
+		panic(err)
+	}
+	return spop
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <binary file>")
+	}
+	spop := parse(os.Args[1])
+	fmt.Printf("Frame Length: %d\n", spop.FrameLen)
 }
